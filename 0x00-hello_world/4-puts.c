424889ad55d2c0ecad8fecf7110e1ea7b7baef7f
Oseni Matthew <osenimatthew9@gmail.com>
2023-10-05 14:41:46 -0700

    nice work today

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+:wq#include<studio.h>
+/**
+* main - Entery point
+*
+* Return: Alway 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<studio.h>
+/**
+*  main - Entery point
+*
+* Return: Alway 0 (success)
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}			
