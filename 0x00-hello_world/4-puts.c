08e61fac08c24eeb1d442c9d1b8916120cad03d8
Oseni Matthew <osenimatthew9@gmail.com>
2023-10-05 15:05:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<studio.h>
 /**
-* main - Entery point
-*
-* Return: Alway 0 (success)
-*/
+ * main - Entery point
+ *
+ * Return: Alway 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
