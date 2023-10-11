5d4c43d26503f373d7e8db304061f2ab9bed61ff
Oseni Matthew <osenimatthew9@gmail.com>
2023-10-11 09:10:52 -0700

    ./11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,25 +8,26 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
 
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d ", i);
-			else if (i == 98)
+				printf("%d, ", i);
+			else
 				printf("%d\n", i);
 		}
-	} else if (n >= 98)
+	}
+	else
 	{
-		for (j = n; j >= 98; j--)
+		for (i = n; i >= 98; i--)
 		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
 		}
 	}
 }
